d5990e4bf415df4128bdd0ae4bd09e878fb37b46
Lehner Florian <dev@der-flo.net>
2017-03-19 21:31:15 +0100

    Apply changed API
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- test/calculus.c
+++ test/calculus.c
@@ -1,16 +1,36 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <calculus.h>
 
 int main (int argc, char **argv)
 {
         unsigned int            flags = 0;
+        double                  result = 0.0;
 
-        parse (flags, "1 + 2");
-        parse (flags, "1 * 2");
-        parse (flags, "1 / 2");
-        parse (flags, "1 ** 2");
-        parse (flags, "gcd(7 21)");
-        parse (flags, "lcm(7 22)");
+        if (parse (flags, &result, "1 + 2") == 0)
+        {
+                fprintf(stdout, "%f\n", result);
+        }
+        if (parse (flags, &result, "1 * 2") == 0)
+        {
+                fprintf(stdout, "%f\n", result);
+        }
+        if (parse (flags, &result, "1 / 2") == 0)
+        {
+                fprintf(stdout, "%f\n", result);
+        }
+        if (parse (flags, &result, "2 ** 2") == 0)
+        {
+                fprintf(stdout, "%f\n", result);
+        }
+        if (parse (flags, &result, "gcd(7 21)") == 0)
+        {
+                fprintf(stdout, "%f\n", result);
+        }
+        if (parse (flags, &result, "lcm(7 22)") == 0)
+        {
+                fprintf(stdout, "%f\n", result);
+        }
 
         return EXIT_SUCCESS;
 }
